4802af894eab212b2f740f1e60773d2c767eb950
Rita <margo6662005@mail.ru>
2019-11-21 17:43:01 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //
+        List <Integer> nums = Arrays.asList(13, 35, 78,987,345, 87764, 7666);
+
+    }
+}
